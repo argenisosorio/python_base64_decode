ac1890deaf60c2f42e2c10f0416e3b517c882029
aosorio <argenisosorio580@hotmail.com>
2017-12-12 08:18:00 -0400

    Arreglando la documentación del código

--- x.py
+++ x.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Decodificar y Codificar en un solo script usando base64 en python
+Codificar y Decodificar en un solo script usando base64 en python
 """
 import base64 # Importamos la librería base64 de python
 
-# Paso de la decodificación
+# Paso de la codificación
 
-with open("1.png", "rb") as f: # Leemos la imagen que vamos a decodificar.
+with open("1.png", "rb") as f: # Leemos la imagen que vamos a codificar.
     data = f.read()
-    data1 = data.encode("base64") # Decodificamos a base64 la imagen.
-    print data1 # cadena de texto ASCII que es la imagen decodificada para visualizarla.
+    data1 = data.encode("base64") # Codificamos a base64 la imagen.
+    print data1 # Cadena de texto ASCII que es la imagen codificada, imprimimos por pantalla para ver la cadena de caracteres.
 
-    # Paso de la codificación
+    # Paso de la decodificación
 
-    imgdata = base64.b64decode(data1) # Codificamos la cadena de caracteres.
-    filename = '2.png' # Imagen codificada a partir de la cadena de texto ASCII que es la imagen decodificada.
+    imgdata = base64.b64decode(data1) # Decodificamos la cadena de caracteres.
+    filename = '2.png' # Imagen decodificada a partir de la cadena de texto ASCII que es la imagen codificada.
     with open(filename, 'wb') as f: # Asignamos un nombre a la nueva imagen.
         f.write(imgdata) # Creamos la imagen y la guardamos en el directorio del script.
